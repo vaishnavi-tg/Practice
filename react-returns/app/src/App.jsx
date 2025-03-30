38cf67c927bdbe9de3d1e61b0fe7d4c42a7fe34a
Vaishnavi T.G <vaishnavitg2@gmail.com>
2025-03-30 12:08:28 +0530

    Added how react component returns

---
+++ react-returns/app/src/App.jsx
@@ -0,0 +1,20 @@
+function App(){
+  return(
+    <div>
+    <Header title="vaishnavi"/>
+    <Header title="vaishnavi2"/>
+    </div>
+
+
+  )
+}
+
+
+function Header({title}){
+  return(
+    <div>
+      {title}
+    </div>
+  )
+}
+export default App
