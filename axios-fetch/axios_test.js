4071d4bf662cd83e0d273ab4afc7f45b5946265c
Vaishnavi T.G <vaishnavitg2@gmail.com>
2025-04-05 19:55:13 +0530

    Added axios-fetch

--- axios-fetch/axios_test.js
+++ axios-fetch/axios_test.js
@@ -1,6 +1,30 @@
 import axios from "axios"
-
+// This is for get
 async function main(){
     response=await axios.get("........")
+    console.log(response.data.todos.length)
+}
+
+// This is for post
+async function main(){
+    response=await axios.post("........")
+    console.log(response.data.todos.length)
+}
+
+// This is for put
+
+async function main(){
+    response=await axios.put("........",{
+        Body:"Bearer 123",
+
+    }
+    )
+    console.log(response.data.todos.length)
+}
+
+// This is for delete
+
+async function main(){
+    response=await axios.delete("........")
     console.log(response.data.todos.length)
 }
--- axios-fetch/fetch_test.js
+++ axios-fetch/fetch_test.js
@@ -1,11 +1,20 @@
 function main() {
-    fetch("www.google.com")
+    fetch("https://www.google.com", {
+        method: "POST",
+        body:{
+            username:"vaishnavi",
+            password:"123467"
+        },
+        headers: {
+            "Authorization": "Bearer",
+        }
+    })
         .then(async (response) => {
             const json = await response.json()
         })
 }
 
-async function main(){
+async function main() {
     const response = await fetch("......")
     const json = await response.json()
     console.log(json.todos.length)
