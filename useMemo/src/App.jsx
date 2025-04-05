1c12cbc4d709f2a60e410c35bafefa0599d194bc
Vaishnavi T.G <vaishnavitg2@gmail.com>
2025-04-05 11:27:53 +0530

    Added useMemo example

---
+++ useMemo/src/App.jsx
@@ -0,0 +1,28 @@
+import { useMemo } from "react"
+import { useState } from "react"
+
+function App() {
+  const [counter, setCounter] = useState(0)
+  const [inputValue, setInputValue] = useState(1)
+  let count = useMemo(() => {
+    let count = 0
+    for (let i = 1; i <= inputValue; i++) {
+      count = count + i
+    }
+    return count
+
+  },[inputValue])
+
+  return <div>
+    <input onChange={function (e) {
+      setInputValue(Number(e.target.value))
+    }} placeholder={"Find sum from 1 to n"}></input>
+    <br></br>
+    Sum from 1 to {inputValue} is {count}
+    <br></br>
+    <button onClick={() => {
+      setCounter(counter + 1)
+    }}>Counter({counter})</button>
+  </div>
+}
+export default App
